110a2d0d9846413f0f4429f798640a45382f219c
Preyas Mistry <preyasrajeshmistry@gmail.com>
2024-12-26 02:58:52 +0530

    eighth commit

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -5,8 +5,9 @@ export default function Skills() {
     "C++", "JavaScript", "TypeScript", "HTML", "CSS", "ReactJS", "NodeJS", 
     "NextJS", "PostgreSQL", "MongoDB", "Figma", "Procreate", "DaVinci Resolve",
     "Bash Script", "AWS", "Google Cloud", "Express.js", "EJS", "JWT", "NPM", 
-    "Nodemon", "jQuery", "MySQL", "TailwindCSS", "Notion", "Postman", "Adobe",
-    "Adobe Lightroom", "Vercel", "Nginx", "Canva", "Git", "GitHub",
+    "Nodemon", "jQuery", "MySQL", "TailwindCSS", "Notion", "Postman", "Adobe fresco",
+    "Adobe Lightroom", "Vercel", "Nginx", "Canva", "Git", "GitHub", "bootstrap", "docker", 
+    "xcode", 
   ];
 
   return (

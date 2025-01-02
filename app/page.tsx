e3a73d342514f530fb07d87872425c36f04e459e
Preyas Mistry <preyasrajeshmistry@gmail.com>
2025-01-02 20:32:38 +0530

    2

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Skills from '../components/Skills'
 import Experience from '../components/Experience'
 import Projects from '../components/Projects'
 import Contact from '../components/Contact'
-import Options from '../components/Options'
+import Options from './components/Options'
 import Artgallary from './artgallary/page'
 
 export default function Home() {
